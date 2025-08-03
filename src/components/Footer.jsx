f2f109c02a6e027b39858ff0c6c8e2baaeb8f6cd
Muhammad Moeen <mooenmalik374@gmail.com>
2025-08-02 20:02:37 -0700

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className=" w-screen bg-violet-300 py-4 text-black ">
+    <footer className=" w-screen bg-violet-300 py-4 px-4 text-black ">
       <div className=" container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row ">
         <p className=" text-center text-sm md:text-left ">
           &copy; LazyDev 2025. All rights reserved
@@ -39,7 +39,12 @@ const Footer = () => {
             </a>
           ))}
         </div>
-        <a href="#privacy-policy" className=" text-center text-sm hover:underline md:text-right">Privacy Policy</a>
+        {/* <a
+          href="#privacy-policy"
+          className=" text-center text-sm hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a> */}
       </div>
     </footer>
   );
